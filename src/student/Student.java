79812cd4e0f9315c1fc09c4bfb4a8505b6219ec5
Darko <darko.dumanjic@gmail.com>
2018-06-08 19:34:40 +0200

    Vezba2_Darko_Dumanjic

--- src/student/Student.java
+++ src/student/Student.java
@@ -5,8 +5,20 @@ public class Student {
     private String firstName;
     private String lastName;
     private int yearOfBirth;
-    private String cours;
-    
+
+    public Student() {
+        this.firstName = "not defined";
+        this.lastName = "not defined";
+        this.yearOfBirth = 0;
+
+    }
+
+    public Student(String firstName, String lastName, int yearOfBirth, String cours) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
@@ -31,13 +43,13 @@ public class Student {
     public int getYearOfBirth() {
         return this.yearOfBirth;
     }
-    
-    public void info(){
+
+    public void info() {
         System.out.println("First name is: " + getFirstName());
         System.out.println("Last name is: " + getLastName());
         System.out.println("Year of birth is: " + getYearOfBirth());
     }
-    
+
     public String getCours() {
         return this.cours;
     }
